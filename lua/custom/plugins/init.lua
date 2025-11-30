fc537c460db779d7fd3e3058a0c78893641bf408
GabHK-DEV <45508528+GabHK-DEV@users.noreply.github.com>
2025-11-30 14:39:58 +0100

    Update plugins
    I've updated the plugins, so they're now listed
    correctly.

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,10 +3,38 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  'RRethy/base16-nvim',
-  priority = 1000, -- Load before other plugins
-
-  'catppuccin/nvim',
-  name = 'catppuccin',
-  priority = 1000,
+  {
+    'RRethy/base16-nvim',
+    priority = 1000, -- Load before other plugins
+  },
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+  },
+  {
+    'nvim-orgmode/orgmode',
+    event = 'VeryLazy',
+    ft = { 'org' },
+    config = function()
+      -- Setup orgmode
+      require('orgmode').setup {
+        org_agenda_files = '~/orgfiles/**/*',
+        org_default_notes_file = '~/orgfiles/refile.org',
+      }
+    end,
+  },
+  {
+    'chipsenkbeil/org-roam.nvim',
+    tag = '0.2.0',
+    ft = { 'org' },
+    dependencies = {
+      'nvim-orgmode/orgmode',
+    },
+    config = function()
+      require('org-roam').setup {
+        directory = '~/org_roam_files',
+      }
+    end,
+  },
 }
